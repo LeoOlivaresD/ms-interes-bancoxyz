df3e44d1c487d2353a6a12e5a293a82ccd4eafa1
LeoOlivaresD <cspraword@gmail.com>
2025-09-12 21:16:44 -0300

    Commit inicial para ms interes

---
+++ src/main/java/com/duoc/ms_interes_bancoxyz/MsInteresBancoxyzApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.ms_interes_bancoxyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsInteresBancoxyzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsInteresBancoxyzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duoc/ms_interes_bancoxyz/MsInteresBancoxyzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.ms_interes_bancoxyz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsInteresBancoxyzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
